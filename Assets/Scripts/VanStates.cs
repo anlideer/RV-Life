a9f39c07d0d09b84acfb957410d11088ead98dbf
anlideer <anlideer@gmail.com>
2022-03-24 23:52:26 +0200

    water tank system

---
+++ Assets/Scripts/VanStates.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanStates : MonoBehaviour
+{
+    public static float waterTank = 1f;
+    public static float greyTank = 0.5f;
+    public static float blackTank = 0.5f;
+}
--- Assets/Scripts/VanUI.cs
+++ Assets/Scripts/VanUI.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class VanUI : MonoBehaviour
 {
+    public float destroyDuration = 3f;
     public GameObject waterPanel;
+    public Text popUpPrefab;
+
+    private Transform canvas;
 
     private void Start()
     {
+        canvas = GameObject.FindGameObjectWithTag("MainCanvas").transform;
         waterPanel.SetActive(false);
     }
 
@@ -25,21 +30,37 @@ public class VanUI : MonoBehaviour
     public void RefillWaterBox(float duration)
     {
         waterPanel.SetActive(false);
+        Text popUp = Instantiate(popUpPrefab, canvas);
+        popUp.text = "Refilling water tank...";
+        Destroy(popUp.gameObject, destroyDuration);
+        GlobalStates.currentTime.TimePass(new MyTime(0, 0, 30));
+        VanStates.waterTank = 1f;
     }
 
     public void EmptyGreyBox(float duration)
     {
         waterPanel.SetActive(false);
+        Text popUp = Instantiate(popUpPrefab, canvas);
+        popUp.text = "Emptying grey tank...";
+        Destroy(popUp.gameObject, destroyDuration);
+        GlobalStates.currentTime.TimePass(new MyTime(0, 0, 30));
+        VanStates.greyTank = 0f;
     }
 
     public void EmptyBlackBox(float duration)
     {
         waterPanel.SetActive(false);
+        Text popUp = Instantiate(popUpPrefab, canvas);
+        popUp.text = "Emptying black tank...";
+        Destroy(popUp.gameObject, destroyDuration);
+        GlobalStates.currentTime.TimePass(new MyTime(0, 0, 30));
+        VanStates.blackTank = 0f;
     }
 
     public void MaintainWaterSystem(float duration)
     {
         waterPanel.SetActive(false);
+        // TODO: maintain, check the water system
     }
 
 }
